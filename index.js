d130514148c0b19d3f894c3f4868c4993fb1a067
wlewis0991 <wlewis1990@gmail.com>
2026-02-23 09:55:40 -0800

    Check to see if position was visited added

--- index.js
+++ index.js
@@ -6,15 +6,19 @@ class Queue {
     }
     
     enqueue(pos){
-        if(!valid(pos)) return null;
+        if(!valid(pos)){
+            return null
+        } else{
+        visited.push(pos)
         this.path.push(pos)
+        }
 }
 
     dequeue(){
         return this.path.shift()
     }
-}
 
+}
 function valid(pos) {
     if (pos[0] >= 0 && pos[0] <= 7 && pos[1] >= 0 && pos[1] <= 7) {
         return true;
@@ -23,19 +27,35 @@ function valid(pos) {
     }
 }
 
+function knightsMoves(start, end){
+    const path = new Queue();
+    if ((!valid(start) || !valid(end)) || visited.includes(start)){
+        console.log("invalid")
+    }else {
+        path.enqueue(start)
+        console.log(path)
+    }
+}
+
+function checkVisited (pos){
+    const visited = JSON.stringify(visitedLocation)
+    const position = JSON.stringify(pos)
+    if ( visited.indexOf(position) >= 0){
+        console.log("visited")
+    }else {
+        console.log("not visited")
+    }
+}
+
+
+const visitedLocation = [
+    [7,1]
+]
+
 const possibleMoves = [
     [-1, 2], [1, 2], [2, 1], [2, -1],
     [1, -2], [-1, -2], [-2, -1], [-2, 1] 
 ]
 
 
-
-const newPath = new Queue();
-
-newPath.enqueue([3,5])
-newPath.enqueue([7,8])
-newPath.enqueue([5,3]);
-newPath.enqueue([2,3])
-newPath.enqueue([9,3])
-console.log(newPath.dequeue())
-console.log(newPath)
+checkVisited([7, 1])
